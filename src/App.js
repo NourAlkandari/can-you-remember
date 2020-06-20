b4715203ec48c349ddaf731754ee7c8ffdc2aa66
NourAlkandari <noor.alkanderi@gmail.com>
2020-06-20 15:45:07 +0300

    Cards component refactor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from "react";
 import "./App.css";
 //CARDS
-// import cards from "./data";
+import cards from "./data";
 
 //Card back image
 import CardBack from "./images/CardBack.jpg";
+import Card from "./Components/Cards";
 
 function App() {
+  let cardsGrid = cards.map(card => {
+    return <Card key={card.id} card={card} />;
+  });
   return (
     <div className="App border my-5">
       <div className="container">
-        <div className="row">
-          <div className="col-3 my-1">
-            <img
-              className="mx-auto"
-              src={CardBack}
-              height="100%"
-              width="100%"
-              key="back"
-            />
-          </div>
-        </div>
+        <div className="row">{cardsGrid}</div>
       </div>
     </div>
   );
