fefba5f404826a09f4d7fad4f329949a2380a0af
NourAlkandari <noor.alkanderi@gmail.com>
2020-06-20 15:11:58 +0300

    Show a single card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "./App.css";
+//CARDS
+// import cards from "./data";
+
+//Card back image
+import CardBack from "./images/CardBack.jpg";
 
 function App() {
-  return <div className="App border my-5">Place your code here</div>;
+  return (
+    <div className="App border my-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-3 my-1">
+            <img
+              className="mx-auto"
+              src={CardBack}
+              height="100%"
+              width="100%"
+              key="back"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
